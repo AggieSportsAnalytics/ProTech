120f157b49251d823d08f09127677c9e5c09fe05
Vishal Shenoy <vishal.nshenoy@icloud.com>
2025-10-05 17:45:23 -0700

    data start (#5)

--- Frontend/ProTech/src/components/AddDataModal.jsx
+++ Frontend/ProTech/src/components/AddDataModal.jsx
@@ -336,13 +336,6 @@ function AddDataModal({ isModalOpen, setIsModalOpen }) {
 					>
 						ForcePlate
 					</button>
-					<button
-						type="button"
-						onClick={handleClose}
-						className="text-gray-500 hover:text-gray-700 mt-4"
-					>
-						Cancel
-					</button>
 				</div>
 			)}
 
